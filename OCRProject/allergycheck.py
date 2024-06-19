f9d0039cb25983e670734b419863f2e06ced4353
yujin-0219 <165646462+yujin-0219@users.noreply.github.com>
2024-06-19 18:02:29 +0900

    Update allergycheck.py
    체크 리스트 반환

--- OCRProject/allergycheck.py
+++ OCRProject/allergycheck.py
@@ -12,7 +12,7 @@ check_vars = [tk.IntVar() for _ in range(20)]
 check_names = ["달걀(Egg)", "우유(Milk)", "밀(Wheat)", "콩(Soy)", "땅콩(Peanut)", "옥수수(Corn)", "메밀(Buckwheat)", "버섯(Mushroom)", "생선(Fish)", "조개(Shellfish)", "바나나(Banana)", "오징어(Squid)", "두유(Soy Milk)", "딸기(Strawberry)", "고추(Pepper)", "키위(Kiwi)", "게(Crab)", "치즈(Cheese)", "토마토(Tomato)", "새우(Shrimp)"]
 
 # 이미지 파일 경로 리스트 (20개)
-image_files = ["ImageFile/egg.png", "ImageFile/milk.png", "ImageFile/wheat.png", "ImageFile/soy.png", "ImageFile/peanut.png", "ImageFile/corn.png", "ImageFile/buckwheat.png", "ImageFile/mushroom.png", "ImageFile/fish.png", "ImageFile/shellfish.png", "ImageFile/banana.png", "ImageFile/squid.png", "ImageFile/soy_milk.png", "ImageFile/strawberry.png", "ImageFile/pepper.png", "ImageFile/kiwi.png", "ImageFile/crab.png", "ImageFile/cheese.png", "ImageFile/tomato.png", "ImageFile/shrimp.png"]
+image_files = ["../ImageFile/egg.png", "../ImageFile/milk.png", "../ImageFile/wheat.png", "../ImageFile/soy.png", "../ImageFile/peanut.png", "../ImageFile/corn.png", "../ImageFile/buckwheat.png", "../ImageFile/mushroom.png", "../ImageFile/fish.png", "../ImageFile/shellfish.png", "../ImageFile/banana.png", "../ImageFile/squid.png", "../ImageFile/soy_milk.png", "../ImageFile/strawberry.png", "../ImageFile/pepper.png", "../ImageFile/kiwi.png", "../ImageFile/crab.png", "../ImageFile/cheese.png", "../ImageFile/tomato.png", "../ImageFile/shrimp.png"]
 
 # 크기를 조정한 이미지 객체 리스트
 images = []
@@ -20,7 +20,7 @@ images = []
 # 이미지를 로드하고 크기를 조정
 for image_file in image_files:
     img = Image.open(image_file)
-    img = img.resize((64, 64), Image.LANCZOS)  # Image.LANCZOS로 수정
+    img = img.resize((64, 64), Image.LANCZOS)
     img = ImageTk.PhotoImage(img)
     images.append(img)
 
@@ -35,99 +35,21 @@ for i in range(4):
     for j in range(5):
         check_buttons[i * 5 + j].grid(row=i, column=j, sticky='w')
 
-# 각 체크박스에 대한 함수를 정의
-def function_A():
-    print("옵션 A가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
+# checked_allergy 리스트
+checked_allergy = []
 
-def function_B():
-    print("옵션 B가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
+# 체크박스 상태를 업데이트하는 함수
+def update_checked_allergy():
+    global checked_allergy
+    checked_allergy = [check_names[i].split("(")[0] for i, var in enumerate(check_vars) if var.get()]
 
-def function_C():
-    print("옵션 C가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_D():
-    print("옵션 D가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_E():
-    print("옵션 E가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_F():
-    print("옵션 F가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_G():
-    print("옵션 G가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_H():
-    print("옵션 H가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_I():
-    print("옵션 I가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_J():
-    print("옵션 J가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_K():
-    print("옵션 K가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_L():
-    print("옵션 L가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_M():
-    print("옵션 M가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_N():
-    print("옵션 N가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_O():
-    print("옵션 O가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_P():
-    print("옵션 P가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_Q():
-    print("옵션 Q가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_R():
-    print("옵션 R가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_S():
-    print("옵션 S가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-def function_T():
-    print("옵션 T가 선택되었습니다.")
-    # 추가 동작을 여기에 정의
-
-functions = [function_A, function_B, function_C, function_D, function_E, function_F, function_G, function_H, function_I, function_J, function_K, function_L, function_M, function_N, function_O, function_P, function_Q, function_R, function_S, function_T]
-
-# 확인 버튼 클릭 시 체크박스 상태를 확인하고 함수를 호출하는 함수
+# 확인 버튼 클릭 시 체크박스 상태를 확인하는 함수
 def show_checked():
-    checked_any = False
-    for i, var in enumerate(check_vars):
-        if var.get():  # 체크된 경우
-            functions[i]()
-            checked_any = True
-
-    if not checked_any:
+    update_checked_allergy()
+    if not checked_allergy:
         print("bye")
+    else:
+        print("Checked allergies: ", checked_allergy)
 
 # 확인 버튼 생성
 button = tk.Button(root, text="확인", command=show_checked)
@@ -135,3 +57,7 @@ button.grid(row=4, column=0, columnspan=5)
 
 # GUI 루프 시작
 root.mainloop()
+
+# 알러지 리스트 반환
+def get_checked_allergy():
+    return checked_allergy
