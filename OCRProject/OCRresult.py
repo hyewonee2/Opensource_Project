6a37b0ba3855dc0b08cc0999ab3def3196f2a494
hyewonee2 <rlagpdnjs200@naver.com>
2024-06-19 21:11:37 +0900

    ddd

--- OCRProject/OCRresult.py
+++ OCRProject/OCRresult.py
@@ -6,7 +6,6 @@ import pytesseract as pt
 from PIL import Image
 from PyQt5.QtCore import Qt
 import array as np
-#충돌 확인 commit
 
 # Tesseract OCR 실행 파일 경로 설정
 pt.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
