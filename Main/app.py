5204304a15b3624ee75d1824c1177c65897ef709
umcanie <152948978+umcanie@users.noreply.github.com>
2024-06-20 14:37:30 +0900

    Update app.py

--- Main/app.py
+++ Main/app.py
@@ -3,14 +3,12 @@ import os
 import sys
 import cv2
 import pytesseract as pt
-import tkinter as tk
 from tkinter import messagebox
 from allergychecker import AllergyChecker
 from OCRresult import ImageOCRWindow, OCRResultWindow
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QFileDialog
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
-import sys
 
 # 글로벌 변수로 checked_list 선언
 checked_list = []
@@ -31,18 +29,28 @@ class App(tk.Tk):
         image_muslim_path = "ImageFile/muslim.png"
         image_allergies_path = "ImageFile/allergies.png"
 
+        self.image_hindu = tk.PhotoImage(file=image_hindu_path).subsample(3, 3)
+        self.image_muslim = tk.PhotoImage(file=image_muslim_path).subsample(3, 3)
+        self.image_allergies = tk.PhotoImage(file=image_allergies_path).subsample(3, 3)
+
+
         self.image_hindu = tk.PhotoImage(file=image_hindu_path)
         self.image_muslim = tk.PhotoImage(file=image_muslim_path)
         self.image_allergies = tk.PhotoImage(file=image_allergies_path)
 
         # 체크박스 생성 및 이미지 삽입
-        tk.Checkbutton(self, text="힌두교(Hindu)", image=self.image_hindu, variable=self.hindu_checked, compound=tk.RIGHT).pack(anchor=tk.W)
-        tk.Checkbutton(self, text="이슬람교(Muslim)", image=self.image_muslim, variable=self.muslim_checked, compound=tk.RIGHT).pack(anchor=tk.W)
-        tk.Checkbutton(self, text="알러지(Allergies)", image=self.image_allergies, variable=self.allergies_checked, compound=tk.RIGHT).pack(anchor=tk.W)
+        tk.Checkbutton(self, text="힌두교(Hindu)", variable=self.hindu_checked, compound=tk.BOTTOM).grid(row=0, column=0, padx=20, pady=10)
+        tk.Label(self, image=self.image_hindu).grid(row=1, column=0, padx=20, pady=10)
+        
+        tk.Checkbutton(self, text="이슬람교(Muslim)", variable=self.muslim_checked, compound=tk.BOTTOM).grid(row=0, column=1, padx=20, pady=10)
+        tk.Label(self, image=self.image_muslim).grid(row=1, column=1, padx=20, pady=10)
+        
+        tk.Checkbutton(self, text="알러지(Allergies)", variable=self.allergies_checked, compound=tk.BOTTOM).grid(row=0, column=2, padx=20, pady=10)
+        tk.Label(self, image=self.image_allergies).grid(row=1, column=2, padx=20, pady=10)
 
         # 확인 버튼 생성
         button = tk.Button(self, text="확인", command=self.show_checked)
-        button.pack(pady=10)
+        button.grid(row=2, columnspan=3, pady=20)
 
     # 확인 버튼 클릭 시 체크박스 상태를 확인하고 함수를 호출하는 함수
     def show_checked(self):
@@ -72,6 +80,11 @@ class App(tk.Tk):
             root = tk.Tk()
             app3 = AllergyChecker(root)
             root.mainloop()
+            
     # 알러지 리스트 반환
     def get_checked_list(self):
         return checked_list
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
