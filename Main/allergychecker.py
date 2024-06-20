62fd9cdb72419b10b636a143f7551e635a434c71
yujin <jaj219@naver.com>
2024-06-20 14:02:24 +0900

    알러지 영어 체크 리스트 생성

--- Main/allergychecker.py
+++ Main/allergychecker.py
@@ -18,7 +18,7 @@ class AllergyChecker:
         self.check_names = ["달걀(Egg)", "우유(Milk)", "밀(Wheat)", "콩(Soy)", "땅콩(Peanut)", "옥수수(Corn)", "메밀(Buckwheat)", "버섯(Mushroom)", "생선(Fish)", "조개(Shellfish)", "바나나(Banana)", "오징어(Squid)", "두유(Soy Milk)", "딸기(Strawberry)", "고추(Pepper)", "키위(Kiwi)", "게(Crab)", "치즈(Cheese)", "토마토(Tomato)", "새우(Shrimp)"]
 
         # 이미지 파일 경로 리스트 (20개)
-        self.image_files = ["ImageFile/egg.png", "ImageFile/milk.png", "ImageFile/wheat.png", "ImageFile/soy.png", "ImageFile/peanut.png", "ImageFile/corn.png", "ImageFile/buckwheat.png", "ImageFile/mushroom.png", "ImageFile/fish.png", "ImageFile/shellfish.png", "ImageFile/banana.png", "ImageFile/squid.png", "ImageFile/soy_milk.png", "ImageFile/strawberry.png", "ImageFile/pepper.png", "ImageFile/kiwi.png", "ImageFile/crab.png", "ImageFile/cheese.png", "ImageFile/tomato.png", "ImageFile/shrimp.png"]
+        self.image_files = ["imagefile/egg.png", "imagefile/milk.png", "imagefile/wheat.png", "imagefile/soy.png", "imagefile/peanut.png", "imagefile/corn.png", "imagefile/buckwheat.png", "imagefile/mushroom.png", "imagefile/fish.png", "imagefile/shellfish.png", "imagefile/banana.png", "imagefile/squid.png", "imagefile/soy_milk.png", "imagefile/strawberry.png", "imagefile/pepper.png", "imagefile/kiwi.png", "imagefile/crab.png", "imagefile/cheese.png", "imagefile/tomato.png", "imagefile/shrimp.png"]
 
         # 크기를 조정한 이미지 객체 리스트
         self.images = []
@@ -45,12 +45,14 @@ class AllergyChecker:
         self.button = tk.Button(self.root, text="확인", command=self.show_checked)
         self.button.grid(row=4, column=0, columnspan=5)
 
-        # checked_allergy 리스트
+        # checked_allergy 및 checked_png 리스트
         self.checked_allergy = []
+        self.checked_png = []
 
     # 체크박스 상태를 업데이트하는 함수
     def update_checked_allergy(self):
         self.checked_allergy = [self.check_names[i].split("(")[0] for i, var in enumerate(self.check_vars) if var.get()]
+        self.checked_png = [self.check_names[i].split("(")[1].strip(")") for i, var in enumerate(self.check_vars) if var.get()]
 
     # 확인 버튼 클릭 시 체크박스 상태를 확인하는 함수
     def show_checked(self):
@@ -65,7 +67,7 @@ class AllergyChecker:
             app2 = QApplication(sys.argv)
             
             # ImageOCRWindow를 생성하고 모달로 띄우기
-            window = ImageOCRWindow(self.checked_allergy)
+            window = ImageOCRWindow(self.checked_allergy, self.checked_png)  # self.checked_png를 추가
             window.show()
             
             sys.exit(app2.exec_())
@@ -75,3 +77,8 @@ class AllergyChecker:
     def get_checked_allergy(self):
         return self.checked_allergy
 
+# 메인 코드 실행
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = AllergyChecker(root)
+    root.mainloop()
